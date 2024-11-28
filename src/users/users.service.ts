7aac89a681ac86ce1d7a25ed4852a2659626aec7
huu-thien <thiencute180402@gmail.com>
2024-11-28 21:05:16 +0700

    [feat] Implement api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Users } from '@/users/entities/user.entity'
+import { Not, Repository } from 'typeorm'
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+    private readonly usersRepository: Repository<Users>
+  ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return 'This action adds a new user'
   }
 
   findAll() {
-    return `This action returns all users`;
+    return `This action returns all users`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<Users> {
+    const user = await this.usersRepository.findOne({
+      where: { id },
+      select: ['id', 'email', 'name', 'phoneNumber', 'birthday', 'hobby', 'gender', 'avatar', 'role', 'updatedAt']
+    })
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`)
+    }
+    return user
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<Users> {
+    try {
+      const user = await this.findOne(id)
+      if (updateUserDto.email) {
+        const existingUser = await this.usersRepository.findOne({
+          where: { email: updateUserDto.email, id: Not(id) }
+        })
+        if (existingUser) {
+          throw new BadRequestException(`Email: ${updateUserDto.email} is already taken`)
+        }
+      }
+      Object.assign(user, updateUserDto)
+      const updatedUser = await this.usersRepository.save(user)
+      return updatedUser
+    } catch (error) {
+      throw new Error(`Error updating user: ${error.message}`)
+    }
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return `This action removes a #${id} user`
   }
 }
