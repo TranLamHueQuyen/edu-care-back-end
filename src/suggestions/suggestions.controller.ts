29e1482cb991c6ddf8fa0145765ab90b210b9e95
huu-thien <thiencute180402@gmail.com>
2024-11-29 21:19:47 +0700

    Æ[feat] implement api get suggestion by depression level

--- src/suggestions/suggestions.controller.ts
+++ src/suggestions/suggestions.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SuggestionsService } from './suggestions.service';
-import { CreateSuggestionDto } from './dto/create-suggestion.dto';
-import { UpdateSuggestionDto } from './dto/update-suggestion.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common'
+import { SuggestionsService } from './suggestions.service'
+import { CreateSuggestionDto } from './dto/create-suggestion.dto'
+import { UpdateSuggestionDto } from './dto/update-suggestion.dto'
+import { DepressionLevel } from '@/suggestions/enum/suggestions.enum'
 
 @Controller('suggestions')
 export class SuggestionsController {
@@ -9,26 +10,27 @@ export class SuggestionsController {
 
   @Post()
   create(@Body() createSuggestionDto: CreateSuggestionDto) {
-    return this.suggestionsService.create(createSuggestionDto);
+    return this.suggestionsService.create(createSuggestionDto)
   }
 
   @Get()
-  findAll() {
-    return this.suggestionsService.findAll();
+  findOneByDepressionLevel(@Query('depressionLevel') depressionLevel: DepressionLevel) {
+    console.log('depressionLevel', depressionLevel)
+    return this.suggestionsService.findOneByDepressionLevel(depressionLevel)
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.suggestionsService.findOne(+id);
+    return this.suggestionsService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateSuggestionDto: UpdateSuggestionDto) {
-    return this.suggestionsService.update(+id, updateSuggestionDto);
+    return this.suggestionsService.update(+id, updateSuggestionDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.suggestionsService.remove(+id);
+    return this.suggestionsService.remove(+id)
   }
 }
--- src/suggestions/suggestions.module.ts
+++ src/suggestions/suggestions.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { SuggestionsService } from './suggestions.service';
-import { SuggestionsController } from './suggestions.controller';
+import { Module } from '@nestjs/common'
+import { SuggestionsService } from './suggestions.service'
+import { SuggestionsController } from './suggestions.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Suggestions } from '@/suggestions/entities/suggestion.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Suggestions])],
   controllers: [SuggestionsController],
-  providers: [SuggestionsService],
+  providers: [SuggestionsService]
 })
 export class SuggestionsModule {}
